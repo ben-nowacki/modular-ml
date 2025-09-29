d38ea0453085caef2e47e06b437b1365db5a7dc1
Ben Nowacki <benjamin.nowacki@uconn.edu>
2025-09-29 14:56:22 -0400

    Bug fixes with PairedSampler logic

--- modularml/core/samplers/__init__.py
+++ modularml/core/samplers/__init__.py
@@ -1,7 +1,9 @@
+from .condition import SimilarityCondition
 from .paired_sampler import PairedSampler
 from .simple_sampler import SimpleSampler
 
 __all__ = [
     "PairedSampler",
+    "SimilarityCondition",
     "SimpleSampler",
 ]
